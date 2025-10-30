2d8983a07ad6b9f3338f2f138e4fbdea9255b913
ClaudiaCasol <claudiacasolxxi@alumnes.ilerna.com>
2025-10-30 17:39:22 +0100

    Commit porcentaje
    He añadido la linea 13 con el código para el porcentaje

--- Calculadora.java.java
+++ Calculadora.java.java
@@ -10,6 +10,8 @@ public class Calculadora {
         System.out.println("Resta: " + (a - b));
         System.out.println("Multiplicación: " + (a * b));
         System.out.println("División: " + (a / b));
+        System.out.println("Porcentaje: " + (a % b));
+        
     }
 }
  
